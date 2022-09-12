fbfd1c4ac17f2c44f3a8429e8de7a4e8dc751cd0
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-12 12:44:29 +0530

    login

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,89 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [login, setLogin] = useState(false);
+
+  if (login) {
+    navigate("/dashboard");
+  }
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setLogin(false);
+    axios
+      .post("http://localhost:8080/login", user)
+      .then((res) => {
+        console.log(res);
+        setUser(res.data.user);
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+        setLogin(true);
+        alert("login successful");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <h2> masai lms</h2>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <form onSubmit={handleClick}>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                value={user.email}
+                onChange={handleChange}
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={user.password}
+                onChange={handleChange}
+              />
+            </div>
+
+            <button
+              className="w-100 btn btn-primary btn-lg "
+              type="submit"
+              onClick={handleClick}
+            >
+              Continue
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Singup.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const Singup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUser({ ...user, [id]: value });
+  }
+
+  const [reg, setReg] = useState(false);
+
+  if (reg) {
+    navigate("/home");
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setReg(false);
+    axios
+      .post("https://masailms-clone.herokuapp.com/register", user)
+      .then((res) => {
+        console.log(res.data);
+        setReg(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="HomePage">
+      <div>
+        {/* <img className="img1" src="img/masai.png" alt="" /> */}
+      </div>
+      <div className="inputs">
+        <form onSubmit={handleSubmit}>
+          <h2>Sign Up</h2>
+          <label className="label1">Full Name</label> <br />
+          <input type="text" className="input" placeholder='Enter Name'
+                id="name" onChange={handleChange}
+              />
+          <br />
+          <br />
+          <label className="label1">Email</label> <br />
+          <input
+            className="input"
+            type="email"
+            name=""
+            id="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+          />{" "}
+          <br />
+          <br />
+          <div className="passwordref">
+            <label className="label1">Password</label>
+          </div>
+          <br />
+          <input
+            className="input"
+            type="password"
+            name=""
+            id="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+          />
+          <br /> <br />
+          <button type="Submit" className="signup">
+            SIGN UP
+          </button>
+          <br />
+        </form>
+        <div className="df1111">
+          <p className="aaa">Already have an account?</p>
+          <Link className="aalink" to={`/`}>
+            <p className="login111">Sign In</p>
+          </Link>{" "}
+        </div>
+      </div>
+      <p className="aaaa">
+        © 2022 by Masai School Privacy Policy & Terms and Conditions
+      </p>
+    </div>
+  );
+};
+
+export default Singup;
